d5aa58188338fe352b9a47b3862f88b66e4ae09b
Harris <harrischeng0519@gmail.com>
2024-04-11 21:18:45 +0800

    feat: add pre-populated database json

--- data/coin/src/main/java/com/example/data/coin/room/CoinDao.kt
+++ data/coin/src/main/java/com/example/data/coin/room/CoinDao.kt
@@ -1,6 +1,8 @@
 package com.example.data.coin.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.data.coin.model.Coin
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +11,7 @@ import kotlinx.coroutines.flow.Flow
 interface CoinDao {
     @Query("SELECT * FROM coin")
     fun getAll(): Flow<List<Coin>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(peoples: List<Coin>)
 }
